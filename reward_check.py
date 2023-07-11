13ca5b01f536d1d5bd196f009621bb5fb9bed505
Karina Velasquez <kvelasquez@xpanxion.com>
2023-07-11 19:08:03 -0400

    Retake

--- reward_check.py
+++ reward_check.py
@@ -1,6 +1,13 @@
 
 def find_best_user(file_path):
-    pass #TODO
+    num = 0
+    line = []
+
+    with open(file_path, "r") as file:
+       for line in file:
+           num_str = line.split()[1:]
+           if num < 17
+
 
 
 def test():
